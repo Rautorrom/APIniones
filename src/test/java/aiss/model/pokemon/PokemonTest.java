f5e5333fc3220992a0ee30cecec59abe48cef6e7
R4 <R4@DESKTOP-521MHJF>
2022-08-31 19:38:29 +0200

    Pokemon test

--- src/test/java/aiss/model/pokemon/PokemonTest.java
+++ src/test/java/aiss/model/pokemon/PokemonTest.java
@@ -5,21 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import aiss.model.repository.MapSitiosRepository;
+import aiss.model.pokemon.Pokemon;
+
 
 public class PokemonTest {
 
-	static Pokemon pokemon;
-	static MapSitiosRepository repositorio = MapSitiosRepository.getInstance();
+	static Pokemon pokemon1;
+	static Pokemon[] pokeAll;
+	static MapSitiosRepository repositorio = new MapSitiosRepository();
 	
 	
 	@Test
 	public void testGetPokemon() {
 		
-		String name= "Bulbasaur";
-		Pokemon pokemon= repositorio.getPokemon(name);
+		String nombre1= "Bulbasaur";
+		pokemon1= repositorio.getPokemon(nombre1);
+		Pokemon p = repositorio.getPokemon(pokemon1.getName());
 		
-		assertEquals("The id of the Pokemons do not match", pokemon.getName(), name);
+		assertEquals("The id of the Pokemons do not match", pokemon1.getName(), p.getName());
+		assertEquals("The name of the Pokemons do not match", pokemon1.getGeneration(), p.getGeneration());
 		
+		
+		//Enseñar resultado
+		System.out.println("Nombre pokemon: " +  p.getName());
+		System.out.println("Generacion del pokemon: " +  p.getGeneration());
 	}
-
 }
