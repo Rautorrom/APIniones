827717dbc3b9a6dc7b363f089841fddbbe343e58
bloxio3001 <adrianporrasazahares@gmail.com>
2022-09-04 16:53:30 +0200

    arreglos mínimos

--- src/main/java/aiss/api/resources/SitioResource.java
+++ src/main/java/aiss/api/resources/SitioResource.java
@@ -109,7 +109,7 @@ public class SitioResource {
 		
 
     @POST
-    @Path("/pokemons/{name}")
+    @Path("/{name}")
     public Response postPokemon(@Context UriInfo uriInfo, @PathParam("name") String name) {
     	
     	Pokemon p = aiss.model.repository.MapSitiosRepository.getInstance().getPokemon(name);
